dd89864cc4aa083fd8df763aa4f491a41f2d86e1
Zachary Parikh <parikhzachary@gmail.com>
2024-02-02 10:01:31 -0500

    Init Kino Server

---
+++ src/main/java/dev/parikh/kinoserver/KinoServerApplication.java
@@ -0,0 +1,13 @@
+package dev.parikh.kinoserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KinoServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KinoServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/parikh/kinoserver/KinoServerApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.parikh.kinoserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KinoServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
